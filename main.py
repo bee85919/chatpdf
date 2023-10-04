8a23bb8c24ea84bc3c6e322b7f84c5145a8b8fe0
bee85919 <bee85919@gmail.com>
2023-10-04 14:19:35 +0900

    Add: Chroma

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
+# Loader
 loader = PyPDFLoader("luckyday.pdf")
 pages = loader.load_and_split()
 
+
+# Splitter
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 500,
     chunk_overlap  = 20,
@@ -17,6 +20,16 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 texts = text_splitter.split_documents(pages)
 
+
+# Embedding
 from langchain.embeddings import OpenAIEmbeddings
 
 embeddings_model = OpenAIEmbeddings()
+
+
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+
+
+# Chroma
+db = Chroma.from_documents(texts, embeddings_model)
