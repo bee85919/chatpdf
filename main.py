a1712943ace9f3953bec00dadbbdeebdfa886613
bee85919 <bee85919@gmail.com>
2023-10-04 13:45:13 +0900

    Add text_splitter

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("luckyday.pdf")
 pages = loader.load_and_split()
 
-print(pages[0])
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap  = 20,
+    length_function = len,
+    is_separator_regex = False,
+)
+
+texts = text_splitter.split_documents(pages)
+
+print(texts[0])
